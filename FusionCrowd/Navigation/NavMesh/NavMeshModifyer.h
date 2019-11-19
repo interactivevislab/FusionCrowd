069b494d8996235c8b5ed9b742f8bb2e0a5c8db8
shepkan <shepkanturgorn@gmail.com>
2019-11-19 15:54:18 +0300

    Home node modification

--- FusionCrowd/Navigation/NavMesh/NavMeshModifyer.h
+++ FusionCrowd/Navigation/NavMesh/NavMeshModifyer.h
@@ -11,6 +11,27 @@ namespace FusionCrowd {
 
 	private:
 		NavMesh&  _navmesh;
+
+		int polygonvertices_count = 0;
+		DirectX::SimpleMath::Vector2* polygonvertices;
+
+		int addednodes_count = 0;
+		NavMeshNode* addednodes;
+
+		int addedvertices_count = 0;
+		DirectX::SimpleMath::Vector2* addedvertices;
+
+		int crosspoints_prev_vertex_ids_num = 0;
+		int* crosspoints_prev_vertex_ids;
+
+		NavMeshPoly* _current_poly;
+		bool _leave_up_node;
+
+		void Initialize(FCArray<NavMeshVetrex> & polygon);
+		void FillAddedVertices();
+		void ModifyHomeNode();
+		void Finalize();
+		DirectX::SimpleMath::Vector2 FindCrossPoint(DirectX::SimpleMath::Vector2 v0, DirectX::SimpleMath::Vector2 v1, int& out_prev_cross_id);
 		float DivideNode(NavMeshNode* node, DirectX::SimpleMath::Vector2* v0, DirectX::SimpleMath::Vector2* v1);
 	};
 }
