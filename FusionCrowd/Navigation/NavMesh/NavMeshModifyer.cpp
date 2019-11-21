d0e8853393519bb2e31673da3c61853c52d138d0
shepkan <shepkanturgorn@gmail.com>
2019-11-21 18:32:30 +0300

    Node polygon cutting

--- FusionCrowd/Navigation/NavMesh/NavMeshModifyer.cpp
+++ FusionCrowd/Navigation/NavMesh/NavMeshModifyer.cpp
@@ -13,66 +13,88 @@ namespace FusionCrowd {
 	}
 
 	float NavMeshModifyer::CutPolygonFromMesh(FCArray<NavMeshVetrex> & polygon) {
-		Initialize(polygon);
-		/*Vector2 *v0 = new Vector2(polygon[0].X, polygon[0].Y),
-			*v1 = new Vector2(polygon[1].X, polygon[1].Y);
-		return DivideNode(&_navmesh.nodes[0], v0, v1);*/
 		_current_poly = &_navmesh.nodes[0]._poly;
+		Initialize(polygon);
+		//split node
+		//int res = SplitNode(Vector2(polygon[0].X, polygon[0].Y), Vector2(polygon[1].X, polygon[1].Y));
+
+		//Cut poly from node
 		FillAddedVertices();
-		int res = AddNodes();
-		ModifyHomeNode();
+		int res = CutPolyFromCurrentNode();
+
+		//Cut curve
+		/*
+		FillAddedVertices();
+		int res = CutCurveFromCurrentNode();
+		*/
 		Finalize();
 		return res;
 	}
 
-	void NavMeshModifyer::ModifyHomeNode() {
-		//calculate vertices num
-		int nodes_to_stay = 0;
-		for (int i = 0; i < _current_poly->vertCount; i++) {
-			auto vertex = _navmesh.vertices[_current_poly->vertIDs[i]];
-			if (IsPointUnderLine(polygonvertices[0], polygonvertices[1], vertex, !_leave_up_node)){
-				nodes_to_stay++;
-			}
+	int NavMeshModifyer::SplitNode(Vector2 v0, Vector2 v1) {
+		//TODO remove init
+		addednodes_count = 2;
+		delete[] addednodes;
+		addednodes = new NavMeshNode[2];
+		delete[] addedvertices;
+		addedvertices_count = 2;
+		addedvertices = new Vector2[2];
+		if (v0.x > v1.x) {
+			addedvertices[0] = polygonvertices[1];
+			addedvertices[1] = polygonvertices[0];
 		}
-		nodes_to_stay += 4;
+		else {
+			addedvertices[0] = polygonvertices[0];
+			addedvertices[1] = polygonvertices[1];
+		}
 
-		//transform leaved node
-		unsigned int* updids = new unsigned int[nodes_to_stay];
-		int idsdiff = 0;
+
+		NavMeshNode node0 = NavMeshNode(), node1 = NavMeshNode();
+		int n0size = 2, n1size = 2;
 		for (int i = 0; i < _current_poly->vertCount; i++) {
 			auto vertex = _navmesh.vertices[_current_poly->vertIDs[i]];
-			bool vertexadded = false;
-			if (IsPointUnderLine(polygonvertices[0], polygonvertices[1], vertex, !_leave_up_node)) {
-				updids[i + idsdiff] = _current_poly->vertIDs[i];
-				vertexadded = true;
-			} else {
-				idsdiff--;
+			if (IsPointUnderLine(v0, v1, vertex))
+				n0size++;
+			else
+				n1size++;
+		}
+		node0.setID(_navmesh.nCount);
+		node1.setID(_navmesh.nCount + 1);
+		node0._poly.vertCount = n0size;
+		node0._poly.vertIDs = new unsigned int[n0size];
+		node1._poly.vertCount = n1size;
+		node1._poly.vertIDs = new unsigned int[n1size];
+
+		int added0 = 0, added1 = 0;
+		for (int i = 0; i < _current_poly->vertCount; i++) {
+			auto vertex = _navmesh.vertices[_current_poly->vertIDs[i]];
+			if (IsPointUnderLine(v0, v1, vertex)) {
+				node0._poly.vertIDs[added0] = _current_poly->vertIDs[i];
+				added0++;
 			}
-			if (i == crosspoints_prev_vertex_ids[polygonvertices_count - 1]) {
-				if (vertexadded) {
-					idsdiff++;
-					updids[i + idsdiff] = _navmesh.vCount + addedvertices_count - 1;
-					idsdiff++;
-					updids[i + idsdiff] = _navmesh.vCount;
-					idsdiff++;
-					updids[i + idsdiff] = _navmesh.vCount + 1;
-					idsdiff++;
-					updids[i + idsdiff] = _navmesh.vCount + polygonvertices_count;
-				} else {
-					idsdiff++;
-					updids[i + idsdiff] = _navmesh.vCount + polygonvertices_count;
-					idsdiff++;
-					updids[i + idsdiff] = _navmesh.vCount + 1;
-					idsdiff++;
-					updids[i + idsdiff] = _navmesh.vCount;
-					idsdiff++;
-					updids[i + idsdiff] = _navmesh.vCount + addedvertices_count - 1;
-				}
+			else {
+				node1._poly.vertIDs[added1] = _current_poly->vertIDs[i];
+				added1++;
 			}
 		}
-		delete[] _current_poly->vertIDs;
-		_current_poly->vertIDs = updids;
-		_current_poly->vertCount = _current_poly->vertCount + idsdiff;
+		bool addingque = false;// v0.x > v1.x;
+		if (v0.x == v1.x) {
+			addingque = v0.y > v1.y;
+		}
+		if (addingque) {
+			node1._poly.vertIDs[added1] = _navmesh.vCount;
+			node1._poly.vertIDs[added1 + 1] = _navmesh.vCount + 1;
+			node0._poly.vertIDs[added0 + 1] = _navmesh.vCount;
+			node0._poly.vertIDs[added0] = _navmesh.vCount + 1;
+		} else {
+			node1._poly.vertIDs[added1 + 1] = _navmesh.vCount;
+			node1._poly.vertIDs[added1] = _navmesh.vCount + 1;
+			node0._poly.vertIDs[added0] = _navmesh.vCount;
+			node0._poly.vertIDs[added0 + 1] = _navmesh.vCount + 1;
+		}
+		addednodes[0] = node0;
+		addednodes[1] = node1;
+		return 0;
 	}
 
 	/*Adds all created nodes and vertexes*/
@@ -112,42 +134,47 @@ namespace FusionCrowd {
 	}
 
 	/*Creates arrays*/
-	void NavMeshModifyer::Initialize(FCArray<NavMeshVetrex> & polygon) {
+	int NavMeshModifyer::Initialize(FCArray<NavMeshVetrex> & polygon) {
 		polygonvertices_count = polygon.size();
 		polygonvertices = new Vector2[polygonvertices_count];
-		for (int i = 0; i < polygonvertices_count; i++) {
-			DirectX::SimpleMath::Vector2 v = DirectX::SimpleMath::Vector2(polygon[i].X, polygon[i].Y);
-			polygonvertices[i] = v;
+		if (!IsClockwise(polygon)) {
+			for (int i = 0; i < polygonvertices_count; i++) {
+				Vector2 v = Vector2(polygon[i].X, polygon[i].Y);
+				polygonvertices[i] = v;
+			}
+		} else {
+			for (int i = 0; i < polygonvertices_count; i++) {
+				Vector2 v = Vector2(polygon[i].X, polygon[i].Y);
+				polygonvertices[polygonvertices_count - i - 1] = v;
+			}
 		}
 
-		addednodes_count = polygonvertices_count - 1;;
+		addednodes_count = polygonvertices_count;
 		addednodes = new NavMeshNode[addednodes_count];
 
 		addedvertices_count = polygonvertices_count*2;
-		addedvertices = new DirectX::SimpleMath::Vector2[addedvertices_count];
+		addedvertices = new Vector2[addedvertices_count];
 
 		crosspoints_prev_vertex_ids_num = polygonvertices_count;
 		crosspoints_prev_vertex_ids = new int[polygonvertices_count];
+		return 0;
 	}
 
 	/*Adds additional nodes*/
-	int NavMeshModifyer::AddNodes() {
-		//which side should be leave?
-		_leave_up_node = !IsPointUnderLine(polygonvertices[0], polygonvertices[1], polygonvertices[2]);
-
-		for (int j = 1; j < polygonvertices_count; j++) {
+	int NavMeshModifyer::CutPolyFromCurrentNode() {
+		for (int j = 0; j < polygonvertices_count; j++) {
 			NavMeshNode updnode = NavMeshNode();
-			updnode.setID(_navmesh.nCount + j - 1);
+			updnode.setID(_navmesh.nCount + j);
 			int vert_count = 0;
-			bool node_side0 = !IsPointUnderLine(polygonvertices[j - 1], polygonvertices[j],
-				addedvertices[polygonvertices_count + j]);
-			bool node_side1 = !IsPointUnderLine(polygonvertices[j], polygonvertices[(j + 1) % polygonvertices_count],
-				addedvertices[polygonvertices_count + j - 1]);
+			Vector2 j0vert = polygonvertices[j];
+			Vector2 j1vert = polygonvertices[(j + 1) % polygonvertices_count];
+			Vector2 j2vert = polygonvertices[(j + 2) % polygonvertices_count];
+			bool node_side0 = !IsPointUnderLine(j0vert, j1vert, j2vert);
+			bool node_side1 = IsPointUnderLine(j1vert, j2vert, j0vert);
 			for (int i = 0; i < _current_poly->vertCount; i++) {
 				auto vertex = _navmesh.vertices[_current_poly->vertIDs[i]];
-				if (IsPointUnderLine(polygonvertices[j-1], polygonvertices[j], vertex, node_side0, true) &&
-					IsPointUnderLine(polygonvertices[j], polygonvertices[(j + 1) % polygonvertices_count], vertex, node_side1)
-					&& IsPointUnderLine(polygonvertices[0], polygonvertices[1], vertex, _leave_up_node, true)) {
+				if (IsPointUnderLine(j0vert, j1vert, vertex, node_side0, true) &&
+					IsPointUnderLine(j1vert, j2vert, vertex, node_side1)) {
 					vert_count++;
 				}
 			}
@@ -160,39 +187,139 @@ namespace FusionCrowd {
 			for (int i = 0; i < _current_poly->vertCount; i++) {
 				auto vertex = _navmesh.vertices[_current_poly->vertIDs[i]];
 				bool vertex_added = false;
-				if (IsPointUnderLine(polygonvertices[j - 1], polygonvertices[j], vertex, node_side0, true) &&
-					IsPointUnderLine(polygonvertices[j], polygonvertices[(j + 1) % polygonvertices_count], vertex, node_side1) &&
-					IsPointUnderLine(polygonvertices[0], polygonvertices[1], vertex, _leave_up_node, true)) {
+				if (IsPointUnderLine(j0vert, j1vert, vertex, node_side0, true) &&
+					IsPointUnderLine(j1vert, j2vert, vertex, node_side1)) {
 					updnode._poly.vertIDs[addedids] = _current_poly->vertIDs[i];
 					addedids++;
 					vertex_added = true;
 				}
-				if (i == crosspoints_prev_vertex_ids[j  - 1]) {
+				if (i == crosspoints_prev_vertex_ids[j]) {
 					if (vertex_added) {
-						updnode._poly.vertIDs[addedids] = _navmesh.vCount + polygonvertices_count + j - 1; //cross point
+						updnode._poly.vertIDs[addedids] = _navmesh.vCount + polygonvertices_count + j; //j0j1 crosspoint
 						addedids++;
-						updnode._poly.vertIDs[addedids] = _navmesh.vCount + j; //polygon node
+						updnode._poly.vertIDs[addedids] = _navmesh.vCount + ((j + 1) % polygonvertices_count); //polygon node j1
 						addedids++;
-						updnode._poly.vertIDs[addedids] = _navmesh.vCount + ((j + 1) % polygonvertices_count); //polygon node
+						updnode._poly.vertIDs[addedids] = _navmesh.vCount + ((j + 2) % polygonvertices_count); //polygon node j2
 						addedids++;
-						updnode._poly.vertIDs[addedids] = _navmesh.vCount + polygonvertices_count + j; //cross point
+						updnode._poly.vertIDs[addedids] = _navmesh.vCount + polygonvertices_count + ((j + 1) % polygonvertices_count); //j1j2 crosspoint
 						addedids++;
 					}
 					else {
-						updnode._poly.vertIDs[addedids] = _navmesh.vCount + polygonvertices_count + j - 1;
+						updnode._poly.vertIDs[addedids] = _navmesh.vCount + polygonvertices_count + j; //j0j1 crosspoint
 						addedids++;
-						updnode._poly.vertIDs[addedids] = _navmesh.vCount + j;
+						updnode._poly.vertIDs[addedids] = _navmesh.vCount + ((j + 1) % polygonvertices_count); //polygon node j1
 						addedids++;
-						updnode._poly.vertIDs[addedids] = _navmesh.vCount + ((j + 1) % polygonvertices_count);
+						updnode._poly.vertIDs[addedids] = _navmesh.vCount + ((j + 2) % polygonvertices_count); //polygon node j2
 						addedids++;
-						updnode._poly.vertIDs[addedids] = _navmesh.vCount + polygonvertices_count + j;
+						updnode._poly.vertIDs[addedids] = _navmesh.vCount + polygonvertices_count + ((j + 1) % polygonvertices_count); //j1j2 crosspoint
 						addedids++;
 					}
 				}
 			}
 
-			addednodes[j-1] = updnode;
+			addednodes[j] = updnode;
 		}
+		return 0;
+	}
+
+	int NavMeshModifyer::CutCurveFromCurrentNode() {
+		for (int j = 0; j < polygonvertices_count-2; j++) {
+			NavMeshNode updnode = NavMeshNode();
+			updnode.setID(_navmesh.nCount + j);
+			int vert_count = 0;
+			Vector2 j0vert = polygonvertices[j];
+			Vector2 j1vert = polygonvertices[(j + 1) % polygonvertices_count];
+			Vector2 j2vert = polygonvertices[(j + 2) % polygonvertices_count];
+			bool node_side0 = !IsPointUnderLine(j0vert, j1vert, j2vert);
+			bool node_side1 = IsPointUnderLine(j1vert, j2vert, j0vert);
+			for (int i = 0; i < _current_poly->vertCount; i++) {
+				auto vertex = _navmesh.vertices[_current_poly->vertIDs[i]];
+				if (IsPointUnderLine(j0vert, j1vert, vertex, node_side0, true) &&
+					IsPointUnderLine(j1vert, j2vert, vertex, node_side1)) {
+					vert_count++;
+				}
+			}
+			vert_count += 4;
+			updnode._poly.vertCount = vert_count;
+			updnode._poly.vertIDs = new unsigned int[vert_count];
+
+			int addedids = 0;
+
+			for (int i = 0; i < _current_poly->vertCount; i++) {
+				auto vertex = _navmesh.vertices[_current_poly->vertIDs[i]];
+				bool vertex_added = false;
+				if (IsPointUnderLine(j0vert, j1vert, vertex, node_side0, true) &&
+					IsPointUnderLine(j1vert, j2vert, vertex, node_side1)) {
+					updnode._poly.vertIDs[addedids] = _current_poly->vertIDs[i];
+					addedids++;
+					vertex_added = true;
+				}
+				if (i == crosspoints_prev_vertex_ids[j]) {
+					if (vertex_added) {
+						updnode._poly.vertIDs[addedids] = _navmesh.vCount + polygonvertices_count + j; //j0j1 crosspoint
+						addedids++;
+						updnode._poly.vertIDs[addedids] = _navmesh.vCount + ((j + 1) % polygonvertices_count); //polygon node j1
+						addedids++;
+						updnode._poly.vertIDs[addedids] = _navmesh.vCount + ((j + 2) % polygonvertices_count); //polygon node j2
+						addedids++;
+						updnode._poly.vertIDs[addedids] = _navmesh.vCount + polygonvertices_count + ((j + 1) % polygonvertices_count); //j1j2 crosspoint
+						addedids++;
+					}
+					else {
+						updnode._poly.vertIDs[addedids] = _navmesh.vCount + polygonvertices_count + j; //j0j1 crosspoint
+						addedids++;
+						updnode._poly.vertIDs[addedids] = _navmesh.vCount + ((j + 1) % polygonvertices_count); //polygon node j1
+						addedids++;
+						updnode._poly.vertIDs[addedids] = _navmesh.vCount + ((j + 2) % polygonvertices_count); //polygon node j2
+						addedids++;
+						updnode._poly.vertIDs[addedids] = _navmesh.vCount + polygonvertices_count + ((j + 1) % polygonvertices_count); //j1j2 crosspoint
+						addedids++;
+					}
+				}
+			}
+
+			addednodes[j] = updnode;
+		}
+
+		//add node splited by v0 v1 line
+
+		NavMeshNode updnode = NavMeshNode();
+		updnode.setID(_navmesh.nCount + polygonvertices_count - 2);
+		int vert_count = 0;
+		Vector2 j0vert = polygonvertices[0];
+		Vector2 j1vert = polygonvertices[1];
+		Vector2 j2vert = polygonvertices[2];
+		bool node_side0 = IsPointUnderLine(j0vert, j1vert, j2vert);
+		for (int i = 0; i < _current_poly->vertCount; i++) {
+			auto vertex = _navmesh.vertices[_current_poly->vertIDs[i]];
+			if (IsPointUnderLine(j0vert, j1vert, vertex, node_side0, true)) {
+				vert_count++;
+			}
+		}
+		vert_count += 2;
+		updnode._poly.vertCount = vert_count;
+		updnode._poly.vertIDs = new unsigned int[vert_count];
+
+		int addedids = 0;
+
+		for (int i = 0; i < _current_poly->vertCount; i++) {
+			auto vertex = _navmesh.vertices[_current_poly->vertIDs[i]];
+			if (IsPointUnderLine(j0vert, j1vert, vertex, node_side0, true)) {
+				updnode._poly.vertIDs[addedids] = _current_poly->vertIDs[i];
+				addedids++;
+			}
+			if (i == crosspoints_prev_vertex_ids[polygonvertices_count - 1]) {
+				updnode._poly.vertIDs[addedids] = _navmesh.vCount + polygonvertices_count; //j0j1 crosspoint
+				addedids++;
+				updnode._poly.vertIDs[addedids] = _navmesh.vCount; //polygon node j0
+				addedids++;
+				updnode._poly.vertIDs[addedids] = _navmesh.vCount + 1; //polygon node j0
+				addedids++;
+			}
+		}
+		addednodes[polygonvertices_count - 2] = updnode;
+
+		addednodes_count--;
 		return 0;
 	}
 
@@ -258,10 +385,10 @@ namespace FusionCrowd {
 				) {
 				//is cross point in v0->v1 direction?
 				if (
-					(v1.x > v0.x && xcross > v1.x) ||
-					(v1.x < v0.x && xcross < v1.x) ||
-					(v1.x == v0.x && v1.y> v0.y && ycross > v1.y) ||
-					(v1.x == v0.x && v1.y < v0.y && ycross < v1.y)
+					(v1.x > v0.x && xcross >= v1.x) ||
+					(v1.x < v0.x && xcross <= v1.x) ||
+					(v1.x == v0.x && v1.y> v0.y && ycross >= v1.y) ||
+					(v1.x == v0.x && v1.y < v0.y && ycross <= v1.y)
 					){
 					out_prev_cross_id = i;
 					return Vector2(xcross, ycross);
@@ -272,19 +399,20 @@ namespace FusionCrowd {
 		return Vector2(0, 0);
 	}
 
+	/*Adds crosspoints for polygon cut*/
 	void NavMeshModifyer::FillAddedVertices() {
 		for (int i = 0; i < polygonvertices_count; i++) {
 			addedvertices[i] = polygonvertices[i];
 		}
 		int prev_cross_id = 0;
-		for (int i = 0; i < polygonvertices_count - 1; i++) {
-			Vector2 crosspoint = FindCrossPoint(polygonvertices[i], polygonvertices[i + 1], prev_cross_id);
+		for (int i = 0; i < polygonvertices_count; i++) {
+			Vector2 crosspoint = FindCrossPoint(polygonvertices[i], polygonvertices[(i + 1) % polygonvertices_count], prev_cross_id);
 			addedvertices[polygonvertices_count + i] = crosspoint;
 			crosspoints_prev_vertex_ids[i] = prev_cross_id;
 		}
-		Vector2 crosspoint = FindCrossPoint(polygonvertices[1], polygonvertices[0], prev_cross_id);
+		/*Vector2 crosspoint = FindCrossPoint(polygonvertices[1], polygonvertices[0], prev_cross_id);
 		addedvertices[polygonvertices_count * 2 - 1] = crosspoint;
-		crosspoints_prev_vertex_ids[polygonvertices_count - 1] = prev_cross_id;
+		crosspoints_prev_vertex_ids[polygonvertices_count - 1] = prev_cross_id;*/
 	}
 
 	/*Returns is point under line v0->v1 (or point.x< line.x if line is vertical)*/
@@ -315,5 +443,16 @@ namespace FusionCrowd {
 				return point.y >= k*point.x + c;
 			else
 				return point.y <= k*point.x + c;
+	}
+
+	bool NavMeshModifyer::IsClockwise(FCArray<NavMeshVetrex> & polygon) {
+		float sum = 0;
+		for (int i = 0; i < polygon.size(); i++) {
+			NavMeshVetrex v0 = polygon[i];
+			NavMeshVetrex v1 = polygon[(i+1) % polygon.size()];
+			sum += (v1.X-v0.X)*(v0.Y + v1.Y);
+		}
+
+		return sum > 0;
 	}
 }
--- FusionCrowd/Navigation/NavMesh/NavMeshModifyer.h
+++ FusionCrowd/Navigation/NavMesh/NavMeshModifyer.h
@@ -26,15 +26,17 @@ namespace FusionCrowd {
 		int* crosspoints_prev_vertex_ids;
 
 		NavMeshPoly* _current_poly;
-		bool _leave_up_node;
 
-		void Initialize(FCArray<NavMeshVetrex> & polygon);
+		int CutPolyFromCurrentNode();
+		int SplitNode(Vector2 v0, Vector2 v1);
+		int CutCurveFromCurrentNode();
+
+		int Initialize(FCArray<NavMeshVetrex> & polygon);
 		void FillAddedVertices();
-		void ModifyHomeNode();
-		int AddNodes();
 		int Finalize();
 		bool IsPointUnderLine(Vector2 v0, Vector2 v1, Vector2 point, bool reverse = false, bool strict = false);
-		DirectX::SimpleMath::Vector2 FindCrossPoint(Vector2 v0, Vector2 v1, int& out_prev_cross_id);
+		Vector2 FindCrossPoint(Vector2 v0, Vector2 v1, int& out_prev_cross_id);
+		bool IsClockwise(FCArray<NavMeshVetrex> & polygon);
 	};
 }
 
